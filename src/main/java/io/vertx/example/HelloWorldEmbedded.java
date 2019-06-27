c26b545547bd5f8c870e4ae1f07bf2301c3af3ca
wangchao <watcher.hangzhou@foxmail.com>
2019-06-27 14:53:41 +0800

    1

---
+++ src/main/java/io/vertx/example/HelloWorldEmbedded.java
@@ -0,0 +1,18 @@
+package io.vertx.example;
+
+import io.vertx.core.Vertx;
+
+
+/**
+ * @author <a href="http://tfox.org">Tim Fox</a>
+ */
+public class HelloWorldEmbedded {
+
+    public static void main(String[] args) {
+        // Create an HTTP server which simply returns "Hello World!" to each request.
+        Vertx.vertx().createHttpServer().requestHandler(
+                req -> req.response().end("666785 Hellodsfds  ddd cc cc World!")
+        ).listen(8080);
+    }
+
+}
